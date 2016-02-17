82f3dcfddc18888f2ddcad13c49b218149364e96
AakashParashar <akku120@gmail.com>
2016-02-17 12:38:52 +0530

    Integration of HTML with JAVA using REST

---
+++ src/main/java/com/aakash/test/Try.java
@@ -0,0 +1,40 @@
+package com.aakash.test;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.view.Viewable;
+
+@Path("/launch")
+public class Try {
+	// Viewable view=new
+	// Viewable("http://localhost:8080/thisoneislast/index.html");
+	/*
+	 * @GET public Response getRedirect(@Context ServletContext context) {
+	 * UriBuilder builder = UriBuilder.fromPath(context.getContextPath());
+	 * builder.path("http://localhost:8080/thisoneislast/launch/start"); return
+	 * Response.seeOther(builder.build()).build(); }
+	 */
+
+	@POST
+	@Path("/start")
+	// @Produces("text/html")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response printt(@FormParam("user") String user, @Context ServletContext context) {
+		// return Response.ok(view).build();
+		// return Response.status(200).entity("Username: "+user).build();
+		// return Response.seeOther(builder.build()).build();
+		UriBuilder builder = UriBuilder.fromPath(context.getContextPath());
+		builder.path("/launch.jsp");
+		return Response.seeOther(builder.build()).build();
+	}
+}
